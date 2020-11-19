3d8e83113baf3138c0466a34c7a297665712ee1d
ppxb <317842449@qq.com>
2020-11-19 20:43:48 +0800

    chore: test jenkins

--- src/views/About.vue
+++ src/views/About.vue
@@ -102,7 +102,6 @@
             </div>
             <div>印瑜伽拥有经验丰富的教练团队，致力于会员的瑜伽练习体验</div>
             <div>以最专业的态度和最完美的姿态对待每一位会员</div>
-            <div>In Life In Yoga</div>
             <div>享受生活，享受瑜伽</div>
             <div class="title">Wechat</div>
             <div class="wechat">
