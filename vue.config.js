f9762e06a340281d64248fd85d4bd90bd1f57c18
ppxb <317842449@qq.com>
2020-11-20 10:24:57 +0800

    chore: change the document title

--- vue.config.js
+++ vue.config.js
@@ -1,7 +1,7 @@
 module.exports = {
   chainWebpack: config => {
     config.plugin('html').tap(args => {
-      args[0].title = 'In Yoga'
+      args[0].title = 'In Yoga | 印瑜伽'
       return args
     })
   }
